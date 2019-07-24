e3c39137ca696525a9bc08ad83ab0ea8a771ade8
xiaohai <xiaohaifengbao@126.com>
2019-07-24 22:42:35 +0800

    第二次提交

--- demo/src/cn/xiaohai/demo/Demo01.java
+++ demo/src/cn/xiaohai/demo/Demo01.java
@@ -4,4 +4,8 @@ public class Demo01 {
     public static void main(String[] args) {
         System.out.println("HelloWorld!!");
     }
+
+    public void show() {
+        System.out.println("222222");
+    }
 }
